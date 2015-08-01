b6ea2945fbf3751842c02218ed37dbe5adfa4185
longieirl <jlongieirl@gmail.com>
2015-08-01 23:57:09 +0100

    Update remoteSetup.sh

--- remoteSetup.sh
+++ remoteSetup.sh
@@ -6,4 +6,5 @@
 git clone https://github.com/longieirl/build-with-docker.git build-with-docker
 cd build-with-docker
 git clone https://github.com/SAP/BUILD.git BUILD
-start.sh
+chmod 755 start.sh
+./start.sh
